7fafd1d920fb8dc6e47837b4fc4d633390d62666
Andrei Schaffer <aschaffer@nvidia.com>
2022-07-27 13:34:36 -0500

    How to pass the Array?.

--- src/cunumeric/quantile/quantile.cc
+++ src/cunumeric/quantile/quantile.cc
@@ -14,4 +14,57 @@
  *
  */
 
-// TODO:
+#include "cunumeric/quantile/quantile.h"
+#include "cunumeric/quantile/quantile_template.inl"
+
+namespace cunumeric {
+
+using namespace Legion;
+using namespace legate;
+
+template <QuantileOpCode OP_CODE, LegateTypeCode CODE, int DIM>
+struct QuantileOpImplBody<VariantKind::CPU, OP_CODE, CODE, DIM> {
+  using OP  = QuantileOp<OP_CODE, CODE>;
+  using ARG = typename OP::T;
+  using RES = typename OP::RetT;
+
+  void operator()(OP func,
+                  AccessorWO<RES, DIM> out,
+                  AccessorRO<ARG, DIM> in,
+                  // AccessorRO<ARR_T, ARR_DIM> in_arr,
+                  // how do I pass the Array on which
+                  // quantile input (in) acts? TODO: figure out
+                  // Note: ARR_T, ARR_DIM may differ from the input index-set shape!
+                  const Pitches<DIM - 1>& pitches,
+                  const Rect<DIM>& rect,
+                  bool dense) const
+  {
+    const size_t volume = rect.volume();
+    if (dense) {
+      auto outptr = out.ptr(rect);
+      auto inptr  = in.ptr(rect);
+      for (size_t idx = 0; idx < volume; ++idx)
+        outptr[idx] = func(inptr[idx] /*, in_arr? TODO: figure out */);
+    } else {
+      for (size_t idx = 0; idx < volume; ++idx) {
+        auto p = pitches.unflatten(idx, rect.lo);
+        out[p] = func(in[p] /*, in_arr? TODO: figure out */);
+      }
+    }
+  }
+};
+
+/*static*/ void QuantileOpTask::cpu_variant(TaskContext& context)
+{
+  quantile_op_template<VariantKind::CPU>(context);
+}
+
+namespace  // unnamed
+{
+static void __attribute__((constructor)) register_tasks(void)
+{
+  QuantileOpTask::register_variants();
+}
+}  // namespace
+
+}  // namespace cunumeric
